55dd55c66caba9b435f7c76a65713b347d32cfc6
Haidee <haideram97@gmail.com>
2017-10-04 16:18:33 -0500

    Fix getEntity

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var cb = require('./lib/OCB.js');
 // Configuracion para la conexion con el context broker
 cb.config('http://207.249.127.149',1026,'v2'); 
 cb.testConnect();
-cb.listEntities();
-cb.createEntity({
+//cb.listEntities();
+/*cb.createEntity({
     "id": "Room10",
     "temperature": {
         "metadata": {
@@ -17,17 +17,17 @@ cb.createEntity({
         "value": 26.5
     },
     "type": "Room"
-});
+});*/
 cb.getEntity('Room10');
-cb.updateJSONAttrEntity('Room10', 'temperature', {
+/*cb.updateJSONAttrEntity('Room10', 'temperature', {
     "type": "Float",
     "value": 34.982398
-});
-cb.updateEntityAttributeValue('Room10', 'temperature', 'value', '2')
+});*/
+/*cb.updateEntityAttributeValue('Room10', 'temperature', 'value', '2')
 cb.updateEntityAttrs('Room10', { 
     "temperature": {
         "value": 75,
         "type": "Integer"
     }
 });
-cb.deleteEntity('Room10');
+cb.deleteEntity('Room10');*/
