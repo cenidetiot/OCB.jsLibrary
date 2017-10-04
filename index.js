49b591de88fd92f5a9992b2779bceb9b385ae33d
Haidee <haideram97@gmail.com>
2017-10-04 13:37:10 -0500

    Avance v1.0

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-var cb = require('./OCB.js');
+var cb = require('./lib/OCB.js');
 
-
-/*
-Las pruebas 
-
-*/
-
+// Configuracion para la conexion con el context broker
+cb.config('http://207.249.127.149',1026,'v2'); 
+cb.testConnect();
+cb.listEntities();
 cb.createEntity({
-	/*
-		tu propio NGSI
-	*/
-})
+    "id": "Room10",
+    "temperature": {
+        "metadata": {
+            "accuracy": {
+                "type": "Float",
+                "value": 0.8
+            }
+        },
+        "type": "Float",
+        "value": 26.5
+    },
+    "type": "Room"
+});
+cb.getEntity('Room10');
+cb.updateJSONAttrEntity('Room10', 'temperature', {
+    "type": "Float",
+    "value": 34.982398
+});
+cb.updateEntityAttributeValue('Room10', 'temperature', 'value', '2')
+cb.updateEntityAttrs('Room10', { 
+    "temperature": {
+        "value": 75,
+        "type": "Integer"
+    }
+});
+cb.deleteEntity('Room10');
