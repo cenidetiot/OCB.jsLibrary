4a16dd004ae90bebf8ebbc8250b77772d163fa44
Haidee <haideeram97@gmail.com>
2017-10-06 00:30:26 -0500

    Modify updateEntityAttributeValue, add methods addJSONAttributeToEntity, addTimeStampEntity

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ var cb = require('./lib/OCB.js');
 // Configuracion para la conexion con el context broker
 cb.config('http://207.249.127.149',1026,'v2'); 
 cb.testConnect();
+//MÉTODO NUEVO 
+cb.addJSONAttributeToEntity("Room1",{
+    "pressure":{
+		"value": 90,
+		"type": "Integer"
+	}
+})
+.then((result) => console.log(result))
+.catch((err) => console.log(err))
 
 cb.getEntity("Room10")
 .then((result) => console.log(result))
@@ -12,21 +21,25 @@ cb.listEntities()
 .then((entities) => {console.log(entities)})
 .catch((err) => console.log(err))
 
-cb.updateEntityAttributeValue('Room1', 'temperature', 'value', '2')
+cb.updateEntityAttributeValue('Room1', 'pressure', '10')
 .then((result) => {console.log(result)})
 .catch((err) => console.log(err))
-/*
+
 cb.updateEntityAttrs('Room1', { 
     "temperature": {
         "value": 75,
         "type": "Integer"
     }
-}).then((result) => console.log(result)).catch((err) => console.log(err))
+})
+.then((result) => console.log(result))
+.catch((err) => console.log(err))
 
 cb.updateJSONAttrEntity('Room1', 'temperature', {
     "type": "Float",
     "value": 34.982398
-}).then((result) => console.log(result)).catch((err) => console.log(err))
+})
+.then((result) => console.log(result))
+.catch((err) => console.log(err))
 
 cb.deleteEntity('RoomPrueba2')
 .then((result) => console.log(result))
@@ -37,18 +50,19 @@ cb.getEntity("Room11")
 .catch((err) => console.log(err))
 
 cb.createEntity({
-    "id": "RoomPromise",
+    "id": "Room1",
     "temperature": {
         "metadata": {
             "accuracy": {
                 "type": "Float",
-                "value": 0.8
+                "value": 8.2
             }
         },
         "type": "Float",
-        "value": 26.5
+        "value": 29.9
     },
     "type": "Room"
-}).then((result) => console.log(result))
-.catch((err) => console.log(err))*/
+})
+.then((result) => console.log(result))
+.catch((err) => console.log(err))
 
