00608266c57333ef71094ac959cfe68500f81da7
Haidee <haideeram97@gmail.com>
2017-11-09 21:33:26 -0600

    Eliminando timestamp agragado automaticamente a las entidades

--- index.js
+++ index.js
@@ -43,7 +43,20 @@ cb.getEntityListType('Room')
 .then((entities) => {console.log(entities)})
 .catch((err) => console.log(err))
 */
+/*var query = cb.queryEntitiesOnArea([
+    [18.87574, -99.21975],
+    [18.87681, -99.21932],
+    [18.87697, -99.2203],
+    [18.87656, -99.2202],
+    [18.87627, -99.22014],
+    [18.875874, -99.220155],
+    [18.87574, -99.21975]
+], ".*","Device",true);
+console.log(query);
 
+cb.geoQueryContext(query)
+.then((result) => console.log(result))
+.catch((err) => console.log(err))*/
 cb.queryEntitiesOnArea([
     [18.87574, -99.21975],
     [18.87681, -99.21932],
@@ -93,7 +106,7 @@ cb.deleteEntity('Room1')
 cb.getEntity("Room1")
 .then((result) => console.log(result))
 .catch((err) => console.log(err))
-
+*/
 cb.createEntity({
     "id": "Room4",
     "temperature": {
@@ -109,5 +122,5 @@ cb.createEntity({
     "type": "Room"
 })
 .then((result) => console.log(result))
-.catch((err) => console.log(err))*/
+.catch((err) => console.log(err))
 
