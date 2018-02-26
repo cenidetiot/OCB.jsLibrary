900af649b4686567227ed8d9f33e6a0e876ac8ef
Haidee <haideeram97@gmail.com>
2018-02-26 01:28:07 -0600

    I added CRUD complementary functions

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ cb.config('http://130.206.113.226',1026,'v2')
 .then((result) => console.log(result))
 .catch((err) => console.log(err))
 
+//RETRIEVE API RESOURCES
+cb.retrieveAPIResources()
+.then((result) => console.log(result))
+.catch((err) => console.log(err))
 /*
 //Add new attribute to an entity
 cb.addJSONAttributeToEntity("Room1",{
@@ -31,23 +35,26 @@ cb.listEntities()
 .then((entities) => {console.log(entities)})
 .catch((err) => console.log(err))
 */
-/*
-//MÉTODOS NUEVOS V1.3.0
+
 //Get attributes of an entity
-cb.getEntityAttrs("Room1")
+/*cb.getEntityAttrs("Room1")
 .then((result) => console.log(result))
-.catch((err) => console.log(err))
+.catch((err) => console.log(err))*/
 
 //Get content attribute of an entity
-cb.getAttributeEntity("Room1", "temperature")
+/*cb.getEntityAttribute("Room1", "temperature")
+.then((result) => console.log(result))
+.catch((err) => console.log(err))*/
+
+cb.getEntityAttributeValue("RoomTest", "temperature")
 .then((result) => console.log(result))
 .catch((err) => console.log(err))
 
 //Get entity list per type 
-cb.getEntityListType('Room')
+/*cb.getEntityListType('Room')
 .then((entities) => {console.log(entities)})
-.catch((err) => console.log(err))
-*/
+.catch((err) => console.log(err))*/
+
 /*var query = cb.queryEntitiesOnArea([
     [18.87574, -99.21975],
     [18.87681, -99.21932],
@@ -159,37 +166,48 @@ cb.queryEntitiesOnArea([
 ]
 ],".*","Device",true).then((result) => console.log(JSON.stringify(result)))
 */
+
 /*
 cb.updateEntityAttributeValue('Room1', 'temperature', 16)
 .then((result) => {console.log(result)})
-.catch((err) => console.log(err))
+.catch((err) => console.log(err))*/
 
-cb.updateEntityAttrs('Room1', { 
+/*cb.updateEntityAttrs('Room1', { 
     "temperature": {
         "value": 75,
         "type": "Integer"
     }
 })
 .then((result) => console.log(result))
-.catch((err) => console.log(err))
+.catch((err) => console.log(err))*/
 
-cb.updateJSONAttrEntity('Room1', 'temperature', {
+/*cb.updateJSONAttrEntity('Room1', 'temperature', {
     "type": "Float",
     "value": 34.982398
 })
 .then((result) => console.log(result))
-.catch((err) => console.log(err))
+.catch((err) => console.log(err))*/
 
-cb.deleteEntity('Room1')
+cb.replaceAllEntityAttributes("RoomTest", {
+    "pressure": {
+        "value": 720,
+        "type": "Integer"
+    }
+})
 .then((result) => console.log(result))
 .catch((err) => console.log(err))
 
-cb.getEntity("Room1")
+/*cb.deleteEntity('Room1')
 .then((result) => console.log(result))
-.catch((err) => console.log(err))
-*/
+.catch((err) => console.log(err))*/
+
+/*cb.getEntity("Room1")
+.then((result) => console.log(result))
+.catch((err) => console.log(err))*/
+
+//CREATE ENTITY
 /*cb.createEntity({
-    "id": "Room4",
+    "id": "RoomTest",
     "temperature": {
         "metadata": {
             "accuracy": {
@@ -200,10 +218,20 @@ cb.getEntity("Room1")
         "type": "Float",
         "value": 29.9
     },
+    "pressure": {
+        "value": 720,
+        "type": "Integer"
+    },
     "type": "Room"
 })
 .then((result) => console.log(result))
 .catch((err) => console.log(err))*/
+
+//DELTE ENTITY ATTRIBUTE
+/*cb.deleteEntityAttribute("RoomTest", "pressure")
+.then((result) => console.log(result))
+.catch((err) => console.log(err))*/
+
 /*let query = "?id=.*&type=Device&georel=coveredBy&q=owner==Id owner&geometry=polygon&coords=18.879751306118546,-99.22197723761204;18.87991373199594,-99.22199869528413;18.87996449005033,-99.22190750017762;18.879984793267777,-99.2218270339072;18.879939111025056,-99.22174656763676;18.879893428769883,-99.22165537253022;18.87973100287282,-99.22145152464509;18.8795888800837,-99.22130132094026;18.879390923140832,-99.221076015383;18.87928940666914,-99.22097945585847;18.87893917436966,-99.22117793932557;18.87856356210443,-99.22121012583375;18.878675230703656,-99.22134960070255;18.878776747547473,-99.22145152464509;18.87888841600463,-99.22154808416965;18.87903053938793,-99.22144079580903;18.879203117619838,-99.22140860930085;18.87936554402868,-99.22153199091554;18.87948228791276,-99.22165537253022;18.879614259162025,-99.22181630507114;18.879751306118546,-99.22197723761204&options=keyValues"
 
 cb.getWithQuery(query)
@@ -228,7 +256,125 @@ Y EL NUMERO DE ENTIDADES QUE EXISTEN DE CADA TIPO DE ENTIDAD*/
 .then((result) => console.dir(result))
 .catch((err) => console.log(err))*/
 
+//CREAR UNA SUSCRIPCIÓN
+/*cb.createSubscription({
+	"description": "Alert subscription TEST",
+	"subject": {
+	"entities": [
+  		{
+			"idPattern": ".*",
+			"type": "Alert"
+		}
+	],
+	"condition": {
+		"attrs": [
+  			"id",	
+  			"type",
+  			"category",
+  			"subCategory",
+  			"location",
+  			"address",
+  			"dateObserved",
+  			"validFrom",
+  			"validTo",
+  			"description",
+  			"alertSource",
+  			"data",
+  			"severity"
+		]
+		}
+	},
+	"notification": {
+  		"attrs": [
+  			"id",  
+          	"type",
+            "category",
+            "subCategory",
+            "location",
+            "address",
+            "dateObserved",
+            "validFrom",
+            "validTo",
+            "description",
+            "alertSource",
+            "data",
+            "severity"
+        ],
+		"attrsFormat": "normalized",
+		"http": {
+			"url": "http://service.mx"
+		}
+	},
+	"throttling": 5
+})
+.then((result) => console.log(result))
+.catch((err) => console.log(err))*/
+
 //OBTENER LA LISTA DE SUSCRIPCIONES.
-cb.listSubscriptions()
+/*cb.listSubscriptions()
+.then((result) => console.log(result))
+.catch((err) => console.log(err))*/
+
+//OBTENER UNA SUSCRIPCIÓN
+/*cb.getSubscription("5a83c5463fc4dec59e4ef8e2")
+.then((result) => console.log(result))
+.catch((err) => console.log(err))*/
+
+//INACTIVAR - ACTIVAR SUSCRIPCIÓN
+/*cb.updateSubscriptionStatus("5a81e50a3fc4dec59e4ef8dc", "active")
+.then((result) => console.log(result))
+.catch((err) => console.log(err))*/
+
+//ELIMINAR UNA SUSCRIPCIÓN
+/*cb.deleteSubscription("5a93a9103fc4dec59e4ef8ec")
+.then((result) => console.log(result))
+.catch((err) => console.log(err))*/
+
+//ACTUALIZAR SUSCRIPCIÓN
+cb.updateSubscription("5a93a9063fc4dec59e4ef8eb", {
+	"description": "Alert subscription TEST",
+	"subject": {
+	"entities": [
+  		{
+			"idPattern": ".*",
+			"type": "Alert"
+		}
+	],
+	"condition": {
+		"attrs": [
+  			"id",	
+  			"type",
+  			"location",
+  			"address",
+  			"dateObserved",
+  			"validFrom",
+  			"validTo",
+  			"description",
+		]
+		}
+	},
+	"notification": {
+  		"attrs": [
+  			"id",  
+          	"type",
+            "category",
+            "subCategory",
+            "location",
+            "address",
+            "dateObserved",
+            "validFrom",
+            "validTo",
+            "description",
+            "alertSource",
+            "data",
+            "severity"
+        ],
+		"attrsFormat": "normalized",
+		"http": {
+			"url": "http://crateservice.com"
+		}
+	},
+	"throttling": 5
+})
 .then((result) => console.log(result))
 .catch((err) => console.log(err))
