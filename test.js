f23349a36108f2205c0ba96dd9dd46e71e00b4e6
CENIDET <32332490+cenidetiot@users.noreply.github.com>
2018-10-01 00:50:59 -0500

    changes in the response of status requests

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 
 var ocb  = require("./lib/OCB")
 var fetch = require("node-fetch")
-ocb.config('http://130.206.113.226:1026/v2');
+
+ocb.config('http://130.206.113.226:1026/v2')
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err))
 
 ocb.getWithQuery(
     "?type=Alert&id=Alert:Device_Smartphone_b0234f7b3f365bf3:1518820773476&options=keyValues"
@@ -12,5 +15,9 @@ ocb.getWithQuery(
     .then((body) => {
         console.log(JSON.stringify(body.body));
         console.log(JSON.stringify(body.headers));
-    });
+    })
+    .catch((err) => console.log(err))
 
+ocb.getSubscription("5a83c5463fc4dec59e4ef8e2")
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err))
